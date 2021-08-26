323d27f7c27aaf0e96ed1fc07bde0f48c3e2df82
artvinicius <arthurviny1999@gmail.com>
2021-08-26 17:40:49 -0300

    Conectando ao mongoDB cpm repository e service

---
+++ src/main/java/com/arthurvinicius/mongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.arthurvinicius.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthurvinicius.mongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
