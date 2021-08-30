c8efbbb5b15645a0ad96b868c17e8f850c24cf78
artvinicius <arthurviny1999@gmail.com>
2021-08-30 11:42:04 -0300

    Usando padrão DTO para retornar usuarios

--- src/main/java/com/arthurvinicius/mongo/resources/UserResource.java
+++ src/main/java/com/arthurvinicius/mongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.arthurvinicius.mongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arthurvinicius.mongo.domain.User;
+import com.arthurvinicius.mongo.dto.UserDTO;
 import com.arthurvinicius.mongo.services.UserService;
 
 
@@ -20,8 +22,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
