e6a9191d458978f5a827c504276d8af942059aaf
artvinicius <arthurviny1999@gmail.com>
2021-08-30 17:41:50 -0300

    Obtendo usuario por id e tratamento de exceção

--- src/main/java/com/arthurvinicius/mongo/repository/UserRepository.java
+++ src/main/java/com/arthurvinicius/mongo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.arthurvinicius.mongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String> {
 
 }
--- src/main/java/com/arthurvinicius/mongo/resources/UserResource.java
+++ src/main/java/com/arthurvinicius/mongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +14,24 @@ import com.arthurvinicius.mongo.domain.User;
 import com.arthurvinicius.mongo.dto.UserDTO;
 import com.arthurvinicius.mongo.services.UserService;
 
-
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
 }
